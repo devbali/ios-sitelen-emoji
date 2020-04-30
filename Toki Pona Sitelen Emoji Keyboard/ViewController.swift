aef29654d8785babeeec14cbf1012cd5db944f69
Jai Bali <jaibali@lbl.gov>
2020-04-29 20:06:46 -0700

    Initial Commit

---
+++ Toki Pona Sitelen Emoji Keyboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Toki Pona Sitelen Emoji Keyboard
+//
+//  Created by ARUN BALI on 29/04/20.
+//  Copyright © 2020 Awara Badal Developments. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Toki Pona Sitelen Emoji KeyboardTests/Toki_Pona_Sitelen_Emoji_KeyboardTests.swift
@@ -0,0 +1,36 @@
+//
+//  Toki_Pona_Sitelen_Emoji_KeyboardTests.swift
+//  Toki Pona Sitelen Emoji KeyboardTests
+//
+//  Created by ARUN BALI on 29/04/20.
+//  Copyright © 2020 Awara Badal Developments. All rights reserved.
+//
+
+import XCTest
+@testable import Toki_Pona_Sitelen_Emoji_Keyboard
+
+class Toki_Pona_Sitelen_Emoji_KeyboardTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
